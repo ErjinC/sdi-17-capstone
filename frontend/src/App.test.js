0902836894ea15d13cf00f7d9a5a3880676db244
kevin.rodney.cagle@gmail.com <kevin.rodney.cagle@gmail.com>
2023-08-01 08:20:59 -0700

    branching off to a test branch

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,52 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event' 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  // render(
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // );
+})
+
+test('renders the header when not logged in', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const titleElement = screen.getByText('Lemon Drop - The Vehicle Repo');
+  const statusElement = screen.getByText('You are not logged in');
+  expect(titleElement).toBeInTheDocument();
+  expect(statusElement).toBeInTheDocument();
+});
+
+test('renders the filters section', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const chooseElement = screen.getByText('Choose a Vehicle');
+  const instructionsElement = screen.getByText('Select a vehicle to start filtering!');
+  expect(chooseElement).toBeInTheDocument();
+  expect(instructionsElement).toBeInTheDocument();
+});
+
+test('renders the hamburger menu when not logged in', async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const homeElement = screen.getByText('Home');
+  const loginElement = screen.getByText('Login');
+  const registerElement = screen.getByText('Register');
+
+  await userEvent.click(screen.getByRole('button'))
+  expect(homeElement).toBeInTheDocument();
+  expect(loginElement).toBeInTheDocument();
+  expect(registerElement).toBeInTheDocument();
 });
